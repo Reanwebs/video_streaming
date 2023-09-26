bfab7b2465388801e9602bbbbc3968011c64f31d
RohithER12 <rohither12@outlook.com>
2023-09-26 14:08:37 +0530

    integreted upload video API to api gateway

--- pkg/api/service/service.go
+++ pkg/api/service/service.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"fmt"
 	"io"
 	"net/http"
 	"videoStreaming/pkg/domain"
@@ -12,7 +11,6 @@ import (
 	"videoStreaming/pkg/respository/interfaces"
 	"videoStreaming/pkg/utils"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/google/uuid"
 )
 
@@ -41,63 +39,178 @@ func (c *VideoServer) HealthCheck(ctx context.Context, input *pb.Request) (*pb.R
 }
 
 func (c *VideoServer) UploadVideo(stream pb.VideoService_UploadVideoServer) error {
-
-	var uploadData *pb.UploadVideoRequest
+	var request domain.ToSaveVideo
 	var buffer bytes.Buffer
-
-	uploadData, err := stream.Recv()
-	if err != nil {
-		return err
-	}
-
-	for {
-		chunk, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		_, err = buffer.Write(chunk.Data)
-		if err != nil {
-			return err
-		}
-	}
-
 	fileUID := uuid.New()
 	fileName := fileUID.String()
 	s3Path := "reanweb/" + fileName + ".mp4"
 
-	err = utils.UploadVideoToS3(buffer.Bytes(), s3Path)
-	if err != nil {
-		fmt.Println("Error uploading video to S3:", err)
-		if awsErr, ok := err.(awserr.Error); ok {
-			fmt.Println("AWS Error Code:", awsErr.Code())
-			fmt.Println("AWS Error Message:", awsErr.Message())
+	for {
+		// Receive the next message from the client
+		uploadData, err := stream.Recv()
+		if err == io.EOF {
+			break // End of stream
 		}
-		return err
-	}
-	request := domain.ToSaveVideo{
-		S3Path:      s3Path,
-		UserName:    uploadData.UserName,
-		AvatarId:    uploadData.AvatarId,
-		Title:       uploadData.ThumbnailId,
-		Discription: uploadData.Discription,
-		Interest:    uploadData.Intrest,
-		ThumbnailId: uploadData.ThumbnailId,
+		if err != nil {
+			return err
+		}
+
+		// Process the received message and populate the request struct
+		request = domain.ToSaveVideo{
+			UserName:    uploadData.UserName,
+			AvatarId:    uploadData.AvatarId,
+			Title:       uploadData.Title,
+			Discription: uploadData.Discription,
+			Interest:    uploadData.Intrest,
+			ThumbnailId: uploadData.ThumbnailId,
+		}
+
+		// Append the received data to the buffer
+		_, err = buffer.Write(uploadData.Data)
+		if err != nil {
+			return err
+		}
 	}
 
+	// Upload the video data to S3
+	err := utils.UploadVideoToS3(buffer.Bytes(), s3Path)
+	if err != nil {
+		// Handle the error
+		return err
+	}
+
+	// Set the S3Path in the request
+	request.S3Path = s3Path
+
+	// Create the video record and get the video ID
 	videoID, err := c.Repo.CreateVideoid(request)
 	if err != nil {
-		fmt.Println("Error saving video ID:", err)
+		// Handle the error
 		return err
 	}
 
-	fmt.Println("Video ID:", videoID)
-
+	// Sending a response and closing the sending stream of bytes
 	return stream.SendAndClose(&pb.UploadVideoResponse{
 		Status:  http.StatusOK,
 		Message: "Video successfully uploaded.",
 		VideoId: videoID,
 	})
 }
+
+// func (c *VideoServer) UploadVideo(stream pb.VideoService_UploadVideoServer) error {
+
+// 	var uploadData *pb.UploadVideoRequest
+// 	var buffer bytes.Buffer
+
+// 	uploadData, err := stream.Recv()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	for {
+// 		chunk, err := stream.Recv()
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			return err
+// 		}
+// 		_, err = buffer.Write(chunk.Data)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	fileUID := uuid.New()
+// 	fileName := fileUID.String()
+// 	s3Path := "reanweb/" + fileName + ".mp4"
+
+// 	err = utils.UploadVideoToS3(buffer.Bytes(), s3Path)
+// 	if err != nil {
+// 		fmt.Println("Error uploading video to S3:", err)
+// 		if awsErr, ok := err.(awserr.Error); ok {
+// 			fmt.Println("AWS Error Code:", awsErr.Code())
+// 			fmt.Println("AWS Error Message:", awsErr.Message())
+// 		}
+// 		return err
+// 	}
+// 	request := domain.ToSaveVideo{
+// 		S3Path:      s3Path,
+// 		UserName:    uploadData.UserName,
+// 		AvatarId:    uploadData.AvatarId,
+// 		Title:       uploadData.ThumbnailId,
+// 		Discription: uploadData.Discription,
+// 		Interest:    uploadData.Intrest,
+// 		ThumbnailId: uploadData.ThumbnailId,
+// 	}
+
+// 	videoID, err := c.Repo.CreateVideoid(request)
+// 	if err != nil {
+// 		fmt.Println("Error saving video ID:", err)
+// 		return err
+// 	}
+
+// 	fmt.Println("Video ID:", videoID)
+
+// 	return stream.SendAndClose(&pb.UploadVideoResponse{
+// 		Status:  http.StatusOK,
+// 		Message: "Video successfully uploaded.",
+// 		VideoId: videoID,
+// 	})
+// }
+
+// func (c *VideoServer) UploadVideo(stream pb.VideoService_UploadVideoServer) error {
+// 	var uploadData *pb.UploadVideoRequest
+// 	fmt.Println("\nuploaded data struct empty\n\n", uploadData, "\n\n\n\n.")
+// 	request := domain.ToSaveVideo{
+// 		UserName:    uploadData.UserName,
+// 		AvatarId:    uploadData.AvatarId,
+// 		Title:       uploadData.ThumbnailId,
+// 		Discription: uploadData.Discription,
+// 		Interest:    uploadData.Intrest,
+// 		ThumbnailId: uploadData.ThumbnailId,
+// 	}
+// 	var buffer bytes.Buffer
+// 	for {
+// 		chunk, err := stream.Recv()
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			return err
+// 		}
+// 		_, err = buffer.Write(chunk.Data)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	fileUID := uuid.New()
+// 	fileName := fileUID.String()
+// 	s3Path := "reanweb/" + fileName + ".mp4"
+
+// 	err := utils.UploadVideoToS3(buffer.Bytes(), s3Path)
+// 	if err != nil {
+// 		fmt.Println("Error uploading video to S3:", err)
+// 		if awsErr, ok := err.(awserr.Error); ok {
+// 			fmt.Println("AWS Error Code:", awsErr.Code())
+// 			fmt.Println("AWS Error Message:", awsErr.Message())
+// 		}
+// 		return err
+// 	}
+// 	request.S3Path = s3Path
+// 	videoID, err := c.Repo.CreateVideoid(request)
+// 	if err != nil {
+// 		fmt.Println("Error saving video ID:", err)
+// 		return err
+// 	}
+
+// 	fmt.Println("Video ID:", videoID)
+
+// 	// Sending a response and closing the sending stream of bytes
+// 	return stream.SendAndClose(&pb.UploadVideoResponse{
+// 		Status:  http.StatusOK,
+// 		Message: "Video successfully uploaded.",
+// 		VideoId: videoID,
+// 	})
+// }
--- pkg/domain/video.go
+++ pkg/domain/video.go
@@ -2,7 +2,7 @@ package domain
 
 type Video struct {
 	ID          uint   `gorm:"primarykey;auto_increment"`
-	VideoId     string `gorm:"uniqueIndex;not null"`
+	VideoId     string `gorm:"not null"`
 	Archived    bool   `gorm:"default:false"`
 	S3Path      string `json:"s3path"`
 	UserName    string `json:"userName"`
